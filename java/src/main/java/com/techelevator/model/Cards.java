c23089fb287815ad3fef360ca574a18a8c015252
Nimensio Johnson <Knimensio@yahoo.com>
2024-06-03 13:28:25 -0400

    cards

---
+++ java/src/main/java/com/techelevator/model/Cards.java
@@ -0,0 +1,56 @@
+package com.techelevator.model;
+
+public class Cards {
+
+    private int cardId;
+
+    private String frontQuestion;
+
+    private String backAnswer;
+
+    private String cardImg;
+
+    private int userId;
+
+    public int getCardId() {
+        return cardId;
+    }
+
+    public void setCardId(int cardId) {
+        this.cardId = cardId;
+    }
+
+    public String getFrontQuestion() {
+        return frontQuestion;
+    }
+
+    public void setFrontQuestion(String frontQuestion) {
+        this.frontQuestion = frontQuestion;
+    }
+
+    public String getBackAnswer() {
+        return backAnswer;
+    }
+
+    public void setBackAnswer(String backAnswer) {
+        this.backAnswer = backAnswer;
+    }
+
+    public String getCardImg() {
+        return cardImg;
+    }
+
+    public void setCardImg(String cardImg) {
+        this.cardImg = cardImg;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+}
+
+
