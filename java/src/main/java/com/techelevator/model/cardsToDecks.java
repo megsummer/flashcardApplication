518f9ddc6869d7ddcdf29966cc25f403c659b46b
megsummer <megan.s.trigg@gmail.com>
2024-06-03 13:33:10 -0400

    cards to desks update

---
+++ java/src/main/java/com/techelevator/model/cardsToDecks.java
@@ -0,0 +1,22 @@
+package com.techelevator.model;
+
+public class cardsToDecks {
+private int cardId;
+private int deckId;
+
+    public int getCardId() {
+        return cardId;
+    }
+
+    public void setCardId(int cardId) {
+        this.cardId = cardId;
+    }
+
+    public int getDeckId() {
+        return deckId;
+    }
+
+    public void setDeckId(int deckId) {
+        this.deckId = deckId;
+    }
+}
