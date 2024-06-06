1a9c9875fb198c1b715bba5d945333943d937d05
megsummer <megan.s.trigg@gmail.com>
2024-06-06 10:45:45 -0400

    create decks 1045am

--- vue/src/views/CreateDeckView.vue
+++ vue/src/views/CreateDeckView.vue
@@ -1,18 +1,24 @@
 <template>
+  <form v-on:submit.prevent="createDeck">
  <div class="deck-form">
         <label for="deck-title">Title:</label>
         <input type="text" id="deck-title" v-model="newDeck.title" />
       </div>
 
       <div class="deck-form">
-        <label for="deck-description">Title:</label>
+        <label for="deck-description">Description:</label>
         <input type="text" id="deck-description" v-model="newDeck.description" />
       </div>
 
-
+      <input type="submit" value="Save Deck" v-on:click="resetForm"/>
+      <input type="submit" value="Write New Cards"/> <br>
+      <input type="submit" value="Submit for Admin Approval"/>
+    </form>
 </template>
 
 <script>
+// add a pop-up box 'Deck Saved'//
+
 import deckService from "../services/DeckServices.js";
 
 
@@ -23,13 +29,26 @@ export default {
 
   data(){
     return {
-      newDeck: {
-        title: "", 
-        description: ""
-      }
+      newDeck: [ 
+        { title: "", 
+        description: "" }
+    ]
     };
   },
   methods: {
+
+    resetForm (){
+      this.newDeck = {};
+     // this.$router.push({name: 'getAllDecks'});
+
+    },
+    createDeck() {
+      let deck = {
+        title: this.title,
+        description: this.description
+      }
+      this.newDeck.push(deck);
+    }
     //saving title, description (&img)//
     //after save keep you on same page to add card?
 
