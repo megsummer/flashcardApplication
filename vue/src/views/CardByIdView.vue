c94f9e0370deea25b216435dbb970fd36f649447
Nimensio Johnson <Knimensio@yahoo.com>
2024-06-07 14:59:03 -0400

    study session/deckbyid

--- vue/src/views/CardByIdView.vue
+++ vue/src/views/CardByIdView.vue
@@ -4,7 +4,6 @@
     <h1>CardByID</h1>
     <p>You are inside CardByID if you see this.</p>
     <p></p>
-    <router-link class="nav-button" v-bind:to="{ name: 'cardById' }">Admin Decks</router-link>
   </div>
 </template>
 
--- vue/src/views/DeckByIdView.vue
+++ vue/src/views/DeckByIdView.vue
@@ -4,13 +4,15 @@
     <h1>DeckByID</h1>
     <p>You are inside DeckbyID if you see this.</p>
     <p></p>
-    <router-link class="nav-button" v-bind:to="{ name: 'deckById' }">Admin Decks</router-link>
+    <router-link class="nav-button" v-bind:to="{ name: 'studySession' }">Study Session</router-link>
+    <router-link class="nav-button" v-bind:to="{ name: 'createCard' }">Create Cards</router-link>
   </div>
 </template>
 
 <script>
 
 import NavTool from '@/components/NavTool.vue';
+
 
 export default {
   name: 'DeckById',
