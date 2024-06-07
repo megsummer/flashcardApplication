7c9594e99063c285ba85e566d8402c1ad5ca1c77
Kinsey Christensen <kit.the.avenger@gmail.com>
2024-06-07 15:19:10 -0400

    checking routes

--- vue/src/views/DeckByIdView.vue
+++ vue/src/views/DeckByIdView.vue
@@ -4,13 +4,21 @@
     <h1>DeckByID</h1>
     <p>You are inside DeckbyID if you see this.</p>
     <p></p>
-    <router-link class="nav-button" v-bind:to="{ name: 'deckById' }">Admin Decks</router-link>
+    <router-link class="nav-button" v-bind:to="{ name: 'studySession' }">Study Session</router-link>
+    <router-link class="nav-button" v-bind:to="{ name: 'createCard' }">Create Cards</router-link>
+  
+
+    <div v-for="card in cards" v-bind:key="card.cardId" 
+          v-bind:to="{name: 'cardById', params :{id: card.cardId}}">
+          <DeckIcon v-bind:deck="deck"/> 
+    </div>
   </div>
 </template>
 
 <script>
 
 import NavTool from '@/components/NavTool.vue';
+
 
 export default {
   name: 'DeckById',
