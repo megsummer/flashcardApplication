fe93d4f96cc4c90a8891a5230ef1b32098b8565a
Victor Cushman <Victor@Cushman.org>
2024-06-07 12:20:10 -0400

    Cardview updated

--- vue/src/views/CardView.vue
+++ vue/src/views/CardView.vue
@@ -1,60 +1,63 @@
 <template>
-   <div class="viewCardActions"> </div>
-  <div id=navpanel>
+   <!-- <div class="viewCardActions"> </div>-->
+   <div>
       <NavTool />
   </div>
 
-  <title>  </title>
   <!-- 1) get working to support display a single card first
        2) then add paging thru cards from search cards ?
        3) add branding
   -->
 
-  <div class="loading" v-if="isLoading">Loading...</div>
+  <!-- <div class="loading" v-if="isLoading">Loading...</div>
   <div v-else>
-        <h1>CardView</h1>
-  </div>
-
-
-  <router-link v-for="card in cards" v-bind:key="card.cardId"
-    v-bind:to="{name: 'getCardById'}">
-    <CardIcon v-bind:card="card"/>
-  </router-link>
-
-  <div class="navActions">
-    <!-- return needs to move to navigation bar top or bottom -->
-    <!-- <router-link v-bind:to="{ name: 'HomeView' }">Return to HomeView</router-link> -->
-  </div>
-  
-  <!-- <div id="branding">
-    <header>
-        <h1>
-          <title> Card View - Viewing a Card </title>
-          <img src="../assets/croppedFlipFlashLogo.png" />
-        </h1>
-    </header>
-  </div>
-  -->
-
- <!-- add buttons for previous, next, exit -->
-
-  <div id=card-view>
-    
-    <div id=Question class="question">
-        <h1> {{ cardId }}</h1>  
-        <h1> {{  frontQuestion }}</h1>
-        <h1> {{ backAnswer }}</h1>
-        <h1> {{  cardImg }}</h1>
-        <h1> {{  userId }}</h1>
-        <h1> {{  tags }}</h1>
-
-        <p>Front - Question</p>
-        <p>Type question here.....</p>
-    </div>
-    <div id=Answer class="answer">
+      <h1>CardView</h1>
+      <div id=Question class="question">
+        <h1> {{ card.frontQuestion }}</h1>
+        <h1> {{ card.backAnswer }}</h1>
+        <h1> {{  card.cardImg }}</h1>
+        <h1> {{  card.tags }}</h1>
+        <p> Front - Question</p>
+        <p> Type question here.....</p>
+      </div>
+      <div id=Answer class="answer">
         <p>Back - Answer</p>
         <p>Type answer here....</p>
-    </div>
+       </div>
+       
+      <div id=Tags class="tags">
+        <p>Tags- Type Answer</p>
+        <p>Tags type answer here....</p>
+      </div>
+  </div> -->
+
+  <div class="navActions">
+    <!-- <router-link :to="{ name: 'HomeView' }">Return to HomeView</router-link> -->
+  </div>
+  <div id="create-card">
+    <h2>
+      Create New Card
+
+    </h2>
+      <form v-on:submit.prevent="createCard">
+        <div class="card-form">
+          <label for="card-frontQuestion">Front Question:</label>
+          <input type="text" id="card-frontQuestion" v-model="newCard.frontQuestion" />
+        </div>
+        <div class="card-form">
+          <label for="card-backAnswer">Back Answer:</label>
+          <input type="text" id="card-backAnswer" v-model="newCard.backAnswer"/>
+        </div>
+        <div class="card-form">
+          <label for="card-img">Card Image:</label>
+          <input type="text" id="card-img" v-model="newCard.cardImg" />
+        </div>
+        <div class="card-form">
+          <label for="card-tags">Tags:</label>
+          <input type="text" id="card-tags" v-model="newCard.tags" />
+          <input type="submit" value="Save Card"/>
+        </div>
+      </form>
   </div>
 </template>
 
@@ -73,10 +76,15 @@ export default {
     
     data () {
       return {
-        cards: [], 
-        isLoading: false,
-      };
-    }, 
+          newCard: {
+            userId: "",
+            frontQuestion: "", 
+            backAnswer: "", 
+            cardImg: "",
+            tags: ["string1", "string2"],
+          },
+    };
+  },
     
     methods: {
       handleError(error, verb) {
@@ -88,27 +96,49 @@ export default {
       } else {
         this.$store.commit('SET_NOTIFICATION', "Error " + verb + " card list. Request could not be created.");
       }
-          },
-    
-    getCardById(cardId) {
-        CardServices.getCardById(cardId).then (response=> {
-          this.cards = response.data;
-          this.isLoading = false; 
-        })
-      },
+    },
 
-    computed: {},
-    
-    created () {
-      const cardId ='1';
-      this.getCardById();
+  
+    resetForm (){
+      this.newCard = {
+        userId: "",
+        frontQuestion: "", 
+        backAnswer: "", 
+        cardImg: "",
+        tags: "",
+        };
+       },
 
-      // test for single card to screen for starters
-      // Fetch card data when the component is created
-      }
+    createCard() {
+      console.log(this.newCard);
+      CardServices.createNewCard(this.newCard)
+        .then(response => {
+          if(response.status === 201) {
+            window.alert('Card Added!');
+          }
+          // this.resetForm();
+          }).catch(error => {
+            this.handleError(error, 'adding');
+          });
+        },
+    },
+    created(){
+        this.createCard();
     }
-  }
+  };
 </script>
 
-<style scoped>
+<style >
+.card-form {
+  margin-top: 10px;
+}
+
+.card-form label{
+display: block
+}
+
+.card-form textarea {
+  height: 60px;
+  width: 300px;
+}
 </style>
