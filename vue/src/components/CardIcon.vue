c77bd4f19071a1cde3ced02c9c670a457bef6bde
Nimensio Johnson <Knimensio@yahoo.com>
2024-06-10 11:11:20 -0400

    stlye

--- vue/src/components/CardIcon.vue
+++ vue/src/components/CardIcon.vue
@@ -36,16 +36,28 @@ a{
 
 
 .card{
-  background-color: #1616161e;
+  background-color: rgb(241, 237, 237);
   color: black;
   text-align: top;
   text-transform: none;
   text-decoration: none;
   margin: 10px;
   padding: 35px;
-  border-radius: 25px;
-  width: 100px;
-  height: 100px;
+  border-radius: 20px;
+  width: 125px;
+  height: 125px;
+  border: .2px solid black;
+  box-shadow: 0 4px 8px rgba(0,0,0,0.1);
+  transition: transform .2s, box-shadow .2s;
+}
+.card:hover {
+  transform: translateY(-5px);
+  box-shadow: 0 8px 16px rgba(0, 0, 0, 0.2);
+}
+.card img {
+  max-width: 100%;
+  max-height: 100%;
+  border-radius: 10px;
 }
 
 
--- vue/src/views/adminDeckView.vue
+++ vue/src/views/adminDeckView.vue
@@ -2,6 +2,8 @@
 
 
 <Logo/>
+
+<NavTool/>
   <div class = "loading" v-if="isLoading">Loading...</div>
 
 
@@ -31,13 +33,15 @@ THESE ARE THE ADMIN DECKS
 import DeckService from '../services/DeckServices';
 import DeckIcon from '../components/DeckIcon.vue';
 import Logo from '../components/Logo.vue';
+import NavTool from '../components/NavTool.vue';
 
 
 
 export default {
   components: {
     DeckIcon,
-    Logo
+    Logo,
+    NavTool
 },
   data() {
     return {
@@ -74,7 +78,50 @@ export default {
 
 }
 </script>
+<style scoped>
+.loading{
+  text-align: center;
+  font-size: 2em;
+  margin-top: 20px;
+}
 
-<style>
+form{
+  text-align: center;
+  margin-bottom: 20px;
+}
 
-</style>../services/DeckServices
+
+.grid-container{
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  gap: 10px;
+  flex-grow: 1;
+}
+
+.grid-item{
+  text-decoration: none;
+}
+
+p{
+  text-align: left center;
+  color: black;
+}
+.main-container{
+  display: flex;
+  padding: 10px;
+}
+
+.top-grid-container{
+  display: grid;
+  grid-template-columns: 1fr auto;
+  align-items: center;
+  padding: 10px;
+  gap: 10px;
+  margin-bottom: 20px;
+
+}
+.logo-container{
+  display: flex;
+  justify-content: center;
+}
+</style>
