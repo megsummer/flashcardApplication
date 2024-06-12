c56efe93b8c095dc7564ef804d4c4501e3355772
megsummer <megan.s.trigg@gmail.com>
2024-06-12 11:51:32 -0400

    myDeckByID updates

--- vue/src/views/DeckByIdView.vue
+++ vue/src/views/DeckByIdView.vue
@@ -2,52 +2,48 @@
    <div class="loading" v-if="isLoading">Loading...</div>
     <div v-else>
  
-      <div class="griddy-container"> 
+    <div class="griddy-container"> 
     <NavTool class="nav-tool"/>
+    <Logo class="logo"/>
    
 
-    <div class="deckHead">
+    <div class="deckHeader">
         <h2> Title: {{ localDeck.deckTitle }}</h2>
-        <p>Description: {{ localDeck.deckDescription }}</p></div>
-     <div class = "image">   <img :src="localDeck.coverImg" alt="Deck Image" /></div>
+        <p>Description: {{ localDeck.deckDescription }}</p>
+    </div>
+
+     <div class = "image">   <img :src="localDeck.coverImg" alt="Deck Image" />
+    </div>
+
   <div class = "deckDetails">
       <p></p>
+
       <UpdateDeck :deck="localDeck" />
       <p></p>
-      <router-link class="nav-button" v-bind:to="{ name: 'studySession', params:{id: localDeck.deckId} }"><button>Study Session</button> </router-link>
-      <p></p>
-      <router-link class="nav-button" v-bind:to="{ name: 'createCard' }"><button>Create Cards</button></router-link>
-      <p></p>
-      <button class="nav-button" @click="deleteDeck">Delete Deck</button>
-     </div>
 
-    <div class="cards-in-deck"> 
+      <router-link class="page-button" v-bind:to="{ name: 'studySession', params:{id: localDeck.deckId} }"><button>Study Session</button> </router-link>
+      <p></p>
 
+      <router-link class="page-button" v-bind:to="{ name: 'createCard' }"><button>Create Cards</button></router-link>
+      <p></p>
+
+      <button class="page-button" @click="deleteDeck">Delete Deck</button>
       <button v-on:click="toggleDeleting">Delete Cards From This Deck</button>
-
-      <div id="deleting" class="grid-container" v-if="isDeleting">
-        <div class="grid-item" id="delete-text">Click a card to delete from this deck.</div>       
-        
-        <div class= "grid-item" v-for="card in cards" v-bind:key="card.cardId"
-        v-on:click="deleteCard(card.cardId)">
-        <CardIcon class="deleting-cards" v-bind:card="card"/>
-      </div></div>
-      
-    <div v-else class="grid-container">
-      <router-link class="grid-item" v-for="card in cards" v-bind:key="card.cardId"
-        v-bind:to="{name: 'cardById', params : {id : card.cardId}}">
-        <CardIcon v-bind:card="card"/>
-     </router-link>
-</div>
-
-
-  
-
     </div>
-    <Logo class="logo"/>
-  </div>
-  </div>
+      <div v-if="isDeleting" class="cards-in-deck"> <div id="delete-text" class="grid-item">Click a card to delete from this deck.
+
+      </div> 
+      <div v-for="card in cards" :key="card.cardId" @click="deleteCard(card.cardId)" class="grid-item"> <CardIcon class="deleting-cards" :card="card" /> 
+      </div> 
+    </div> 
+    <div v-else class="cards-in-deck"> <router-link v-for="card in cards" :key="card.cardId" :to="{ name: 'cardById', params: { id: card.cardId } }" class="grid-item"> <CardIcon :card="card" /> </router-link> </div> </div>
+    </div>
+    
+  
+  
 </template>
+
+
 
 <script>
 
@@ -169,6 +165,10 @@ export default {
   }
 };
 </script>
+
+
+
+
 <style>
 .deleting-cards{
   background-color: rgb(245, 130, 130);
@@ -203,17 +203,34 @@ export default {
   grid-area: logo;
   margin-left: auto;
 }
-.deckHead{
-  grid-area: deckHead;
+.deckHeader{
+  grid-area: deckHeader;
+  text-align: center;
+  
 }
 .griddy-container {
   display: grid;
   grid-template-columns: 1fr 3fr 1 fr;
   grid-template-areas: 
-    "nav deckHead  logo"
-    "nav deckDetails image "
+    "nav deckHeader  logo"
+    "nav image deckDetails"
     ". cards-in-deck cards-in-deck";
   gap: 15px;
+}
+
+.page-button{
+  width: 170px;
+  padding: 2px;
+  height: 30px;
+  border: black;
+  background-color: rgb(241, 214, 11);
+  color: black;
+  border-radius: 5px;
+  font-size: 15px;
+  cursor: pointer;
+  transition: background-color .3s ease;
+  font-weight: bold;  
+  
 }
 .nav-tool{
   grid-area: nav;
