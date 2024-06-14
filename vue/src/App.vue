2230dcfbd1efe95ae3d65b933bd6a14427d5f034
Kinsey Christensen <kit.the.avenger@gmail.com>
2024-06-14 08:50:01 -0400

    added tab title

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,6 +1,8 @@
 <template>
+
   <div id="capstone-app">
     <div id="nav">
+      
       <router-link v-bind:to="{ name: 'home' }" v-if="$store.state.token != ''">Home</router-link>&nbsp;&nbsp;
       <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
     </div>
